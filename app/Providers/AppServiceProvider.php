d3c3fcc784e0cf76d897d3762015a6d47a4a9284
Shawn Weigand <shawnweigand1@gmail.com>
2025-09-03 13:03:10 -0400

    Update Environment Configuration and Force HTTPS in Production
    - Changed the VITE_APP_NAME in .env.example to "The Shawn Shop" for branding consistency.
    - Added URL scheme enforcement to HTTPS in AppServiceProvider for production environment security.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
 use Lorisleiva\Actions\Facades\Actions;
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Actions::registerCommands();
         // Cashier::calculateTaxes();
     }
